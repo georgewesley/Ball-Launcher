f81c7d1b5da0d66cbdb43446ca4cbf0fef686327
George Wesley Lyon <georgewesley17@gmail.com>
2022-01-05 17:19:27 -0500

    Initial commit

---
+++ Assets/Scripts/BallHandler.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class BallHandler : MonoBehaviour
+{
+
+    [SerializeField] CircleCollider2D springCollider;
+    [SerializeField] private SpringJoint2D currentBallSpringJoint;
+    [SerializeField] private GameObject ballPrefab;
+    [SerializeField] private Rigidbody2D pivot;
+    [SerializeField] private float respawnTime;
+     CircleCollider2D ballCollider;
+     private Rigidbody2D currentBallRigidBody;
+     Vector2 touchPosition;
+     private Camera mainCamera;
+     private bool isDragging;
+     
+
+     private float gravityScale;
+    void Start()
+    {
+        Respawn();
+        mainCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(currentBallRigidBody == null) { return; }
+        if(!Touchscreen.current.primaryTouch.press.isPressed) {
+            if(isDragging) {
+                currentBallRigidBody.gravityScale = gravityScale;
+                LaunchBall();
+                if(springCollider.IsTouching(ballCollider)) {
+                    DetachBall();
+                }
+            }
+            return;
+        }
+        isDragging = true;
+        currentBallRigidBody.isKinematic = true;
+        touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
+        Vector3 worldPosition = mainCamera.ScreenToWorldPoint(touchPosition);
+        
+        currentBallRigidBody.position = worldPosition;
+
+        
+    }
+    private void LaunchBall() {
+        currentBallRigidBody.isKinematic = false;
+    }
+
+    void DetachBall() {
+        isDragging = false;
+        currentBallRigidBody = null;
+        currentBallSpringJoint.enabled = false;
+        Invoke("Respawn", respawnTime);
+    }
+
+    void Respawn() {
+        currentBallSpringJoint.enabled = true;
+        GameObject ballInstance = Instantiate(ballPrefab, pivot.position, Quaternion.identity);
+        currentBallRigidBody = ballInstance.GetComponent<Rigidbody2D>();
+        currentBallSpringJoint.connectedBody = currentBallRigidBody;
+        ballCollider = ballInstance.GetComponent<CircleCollider2D>();
+        gravityScale = currentBallRigidBody.gravityScale;
+        currentBallRigidBody.gravityScale = 0;
+    }
+
+}
